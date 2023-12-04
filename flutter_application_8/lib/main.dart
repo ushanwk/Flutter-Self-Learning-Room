20e809a7713dce07e8cd68d3ad178cc043f1af6d
UshanKaushalya <Ushanwk22@gmail.com>
2023-12-04 12:25:03 +0530

    Text field developing

--- flutter_application_8/lib/main.dart
+++ flutter_application_8/lib/main.dart
@@ -6,17 +6,16 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Text Field App'),
     );
   }
 }
@@ -36,13 +35,18 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(widget.title, style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 25),),
+        toolbarHeight: 70,
       ),
-      body: Center(
+      body: const Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-
+            Padding(
+              padding: EdgeInsets.all(30.0),
+              child: TextField(
+                maxLength: 8,
+              ),
+            ),
           ],
         ),
       ),
